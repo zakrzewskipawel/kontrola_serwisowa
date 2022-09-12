af2d2f89dc8209649831ebaa7cb36a33af265603
Pawel Zakrzewski <p.zakrzewski.szofer@gmail.com>
2022-09-12 23:06:05 +0200

    my_account_page_content

---
+++ lib/app/features/home/my_account_page_content/my_account_page_content.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kontrola_serwisowa/app/cubit/root_cubit.dart';
+
+
+class MyAccountPageContent extends StatelessWidget {
+  const MyAccountPageContent({
+    Key? key,
+    required this.email,
+  }) : super(key: key);
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Jesteś zalogowany jako $email'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              context.read<RootCubit>().signOut();
+            },
+            child: const Text('Wyloguj'),
+          ),
+        ],
+      ),
+    );
+  }
+}
